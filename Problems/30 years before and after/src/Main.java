4e29c93e0a0122234a5d131198a3cd6f8e799e3c
nomenillislegion <sv.kazhadan@mail.ru>
2020-10-14 21:38:17 +0300

    test

--- Problems/30 years before and after/src/Main.java
+++ Problems/30 years before and after/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         // put your code here
+
         Scanner scanner = new Scanner(System.in);
         LocalDate date = LocalDate.parse(scanner.nextLine());
         System.out.println(date.minusYears(30));
