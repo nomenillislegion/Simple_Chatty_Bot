4b5fae376fa08f8e9ab49d4cb17d988353801077
nomenillislegion <sv.kazhadan@mail.ru>
2020-10-25 13:08:11 +0200

    Static members 25.10.20

---
+++ Problems/Too many cats/src/Main.java
@@ -0,0 +1,24 @@
+class Cat {
+
+    static int counter;
+    String name;
+    int age;
+    // write static and instance variables
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+        counter++;
+        if (counter > 5) {
+            System.out.println("You have too many cats");
+        }
+        // implement the constructor
+        // do not forget to check the number of cats
+    }
+
+    public static int getNumberOfCats() {
+        return counter;
+
+        // implement the static method
+    }
+}
